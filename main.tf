c98d68c2979101500f635731ee365c7ba2839f68
Artem Ushenko <kingart030192kingart@gmail.com>
2024-01-10 12:50:04 +0200

    Changed IAM lambda role policies to cloudwatch full access

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
-# IAM Role for the Lambda function
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_execution_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version   = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
+        Action   = "sts:AssumeRole",
+        Effect   = "Allow",
         Principal = {
           Service = "lambda.amazonaws.com"
         }
@@ -16,30 +15,10 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-# IAM Policy for Lambda logging
-resource "aws_iam_policy" "lambda_logging" {
-  name = "lambda_logging_policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "arn:aws:logs:*:*:*",
-        Effect = "Allow"
-      }
-    ]
-  })
-}
-
-# Attach the policy to the role
-resource "aws_iam_role_policy_attachment" "lambda_logs_attachment" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_logging.arn
+resource "aws_iam_policy_attachment" "cloudwatch_full_access_attachment" {
+  name       = "cloudwatch_full_access_attachment"
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+  roles      = [aws_iam_role.lambda_role.name]
 }
 
 # Lambda function
@@ -49,18 +28,5 @@ resource "aws_lambda_function" "terraform_lambda_func" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "script.lambda_handler"
   runtime       = "python3.8"
-  depends_on    = [aws_iam_role_policy_attachment.lambda_logs_attachment]
-}
-
-# Outputs
-output "teraform_aws_role_output" {
-  value = aws_iam_role.lambda_role.name
-}
-
-output "teraform_aws_role_arn_output" {
-  value = aws_iam_role.lambda_role.arn
-}
-
-output "teraform_logging_arn_output" {
-  value = aws_iam_policy.lambda_logging.arn
+  depends_on    = [aws_iam_policy_attachment.cloudwatch_full_access_attachment]
 }
