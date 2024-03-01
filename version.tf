8e6e49d27f97b3cc93fd340d446a8ddcd87cdd4f
Artem Ushenko <99095504+Artem-Ushenko@users.noreply.github.com>
2024-03-01 19:14:36 +0200

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,3 +1,15 @@
 terraform {
   required_version = ">= 1.6.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.27"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "1.3.0"
+    }
+  }
+
 }
